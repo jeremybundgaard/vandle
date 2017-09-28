d3a6eb5d4301eb6f28688ab39aaac50f50644df5
jeremybundgaard <jeremybundgaard@me.com>
2017-09-28 11:18:33 -0400

    added utkscan run sripts

---
+++ macros/run_17N.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+batch=""
+infile=~/links/data/17N/17N_cern/a17_m31_08-4.ldf
+config=~/links/configs/Config-17N-lowgain.xml
+ldf=`basename $infile .ldf`
+
+array=( "$@" )
+arraylength=${#array[@]}
+for (( i=1; i<${arraylength}+1; i++ ));
+do
+	if [[ "${array[$i-1]}" = "-b" ]]; then
+    batch="-b"
+	fi
+	if [[ "${array[$i-1]}" = "-c" ]]; then
+    config=${array[$i]}
+	fi
+	if [[ "${array[$i-1]}" = "-i" ]]; then
+    infile=${array[$i]}
+		ldf=`basename $infile .ldf`
+	fi
+done
+
+outdir=${ldf}_date_`date +%d%b%y_%H%M.%S`
+mkdir $outdir
+cd $outdir
+
+# echo "~/paass/install/bin/utkscan -i $infile --frequency 250 -f R34300 -c $config -o $ldf"
+# ~/paass/install/bin/utkscan -i $infile --frequency 250 -f R34300 -c $config -o $ldf
+
+echo "~/paass/install/bin/utkscan -i $infile -c $config -o $ldf $batch"
+~/paass/install/bin/utkscan -i $infile -c $config -o $ldf $batch
+
+cd -
