3f2d9c02c8c0e777b17eceec87421c671e48c3af
jeremybundgaard <jeremybundgaard@me.com>
2017-11-02 18:43:57 -0400

    added run_132Cd.sh

---
+++ macros/run_132Cd.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+batch=""
+infile=/data/cern/isoldeData/oct/is600/IS600Oct_a132_01.ldf
+config=~/links/configs/132Cd/132Cd_Config_tkedit.xml
+ldf=`basename $infile .ldf`
+
+array=( "$@" )
+arraylength=${#array[@]}
+for (( i=1; i<${arraylength}+1; i++ ));
+do
+	if [[ "${array[$i-1]}" = "-b" ]]; then
+    batch="-b"
+	fi
+	if [[ "${array[$i-1]}" = "-c" ]]; then
+    config=${array[$i]}
+	fi
+	if [[ "${array[$i-1]}" = "-i" ]]; then
+    infile=${array[$i]}
+		ldf=`basename $infile .ldf`
+	fi
+done
+
+outdir=${ldf}_date_`date +%d%b%y_%H%M.%S`
+mkdir $outdir
+cd $outdir
+
+# echo "~/paass/install/bin/utkscan -i $infile --frequency 250 -f R34300 -c $config -o $ldf"
+# ~/paass/install/bin/utkscan -i $infile --frequency 250 -f R34300 -c $config -o $ldf
+
+echo "~/paass/install/bin/utkscan -i $infile -c $config -o $ldf $batch"
+~/paass/install/bin/utkscan -i $infile -c $config -o $ldf $batch
+
+cd -
